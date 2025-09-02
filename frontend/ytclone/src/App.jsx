9c4b9551ce820e2b2a21bed2731109f2a9393a6f
Dilpreet-47 <singh.dilpreet.flux@gmail.com>
2025-09-02 15:55:00 +0530

    first commit

---
+++ frontend/ytclone/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return(
+    <div className="flex bg-slate-500 h-screen w-screen justify-center items-center">
+      <h1 className="text-3xl text-white">hello world</h1>
+    </div>
+  )
+}
+
+export default App;
